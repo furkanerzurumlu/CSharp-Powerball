5f135f20d2aa7c69af7b6400c094a145dd675479
Furkan Erzurumlu <47275433+furkanerzurumlu@users.noreply.github.com>
2021-01-17 22:09:02 +0300

    Add files via upload

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt6 = new System.Windows.Forms.TextBox();
+            this.txt5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnResult = new System.Windows.Forms.Button();
+            this.lab1 = new System.Windows.Forms.Label();
+            this.lab2 = new System.Windows.Forms.Label();
+            this.lab3 = new System.Windows.Forms.Label();
+            this.lab4 = new System.Windows.Forms.Label();
+            this.lab5 = new System.Windows.Forms.Label();
+            this.lab6 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(199, 88);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(53, 20);
+            this.txt3.TabIndex = 0;
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(39, 88);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(53, 20);
+            this.txt1.TabIndex = 1;
+            // 
+            // txt4
+            // 
+            this.txt4.Location = new System.Drawing.Point(279, 88);
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(53, 20);
+            this.txt4.TabIndex = 2;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(119, 88);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(53, 20);
+            this.txt2.TabIndex = 3;
+            // 
+            // txt6
+            // 
+            this.txt6.Location = new System.Drawing.Point(439, 88);
+            this.txt6.Name = "txt6";
+            this.txt6.Size = new System.Drawing.Size(53, 20);
+            this.txt6.TabIndex = 4;
+            // 
+            // txt5
+            // 
+            this.txt5.Location = new System.Drawing.Point(359, 88);
+            this.txt5.Name = "txt5";
+            this.txt5.Size = new System.Drawing.Size(53, 20);
+            this.txt5.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(98, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "-";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(178, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "-";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(258, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "-";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(338, 88);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "-";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(418, 88);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(15, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "-";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(176, 29);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(191, 31);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "POWERBALL";
+            // 
+            // btnResult
+            // 
+            this.btnResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnResult.Location = new System.Drawing.Point(199, 134);
+            this.btnResult.Name = "btnResult";
+            this.btnResult.Size = new System.Drawing.Size(133, 26);
+            this.btnResult.TabIndex = 12;
+            this.btnResult.Text = "Result";
+            this.btnResult.UseVisualStyleBackColor = true;
+            this.btnResult.Click += new System.EventHandler(this.btnResult_Click_1);
+            // 
+            // lab1
+            // 
+            this.lab1.AutoSize = true;
+            this.lab1.Location = new System.Drawing.Point(98, 191);
+            this.lab1.Name = "lab1";
+            this.lab1.Size = new System.Drawing.Size(0, 13);
+            this.lab1.TabIndex = 13;
+            // 
+            // lab2
+            // 
+            this.lab2.AutoSize = true;
+            this.lab2.Location = new System.Drawing.Point(178, 191);
+            this.lab2.Name = "lab2";
+            this.lab2.Size = new System.Drawing.Size(0, 13);
+            this.lab2.TabIndex = 14;
+            // 
+            // lab3
+            // 
+            this.lab3.AutoSize = true;
+            this.lab3.Location = new System.Drawing.Point(258, 191);
+            this.lab3.Name = "lab3";
+            this.lab3.Size = new System.Drawing.Size(0, 13);
+            this.lab3.TabIndex = 15;
+            // 
+            // lab4
+            // 
+            this.lab4.AutoSize = true;
+            this.lab4.Location = new System.Drawing.Point(332, 191);
+            this.lab4.Name = "lab4";
+            this.lab4.Size = new System.Drawing.Size(0, 13);
+            this.lab4.TabIndex = 16;
+            // 
+            // lab5
+            // 
+            this.lab5.AutoSize = true;
+            this.lab5.Location = new System.Drawing.Point(412, 191);
+            this.lab5.Name = "lab5";
+            this.lab5.Size = new System.Drawing.Size(0, 13);
+            this.lab5.TabIndex = 17;
+            // 
+            // lab6
+            // 
+            this.lab6.AutoSize = true;
+            this.lab6.Location = new System.Drawing.Point(492, 191);
+            this.lab6.Name = "lab6";
+            this.lab6.Size = new System.Drawing.Size(0, 13);
+            this.lab6.TabIndex = 18;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label13.Location = new System.Drawing.Point(12, 184);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(55, 20);
+            this.label13.TabIndex = 19;
+            this.label13.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 257);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.lab6);
+            this.Controls.Add(this.lab5);
+            this.Controls.Add(this.lab4);
+            this.Controls.Add(this.lab3);
+            this.Controls.Add(this.lab2);
+            this.Controls.Add(this.lab1);
+            this.Controls.Add(this.btnResult);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt5);
+            this.Controls.Add(this.txt6);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.txt3);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.Name = "Form1";
+            this.Text = "POWERBALL";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt6;
+        private System.Windows.Forms.TextBox txt5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnResult;
+        private System.Windows.Forms.Label lab1;
+        private System.Windows.Forms.Label lab2;
+        private System.Windows.Forms.Label lab3;
+        private System.Windows.Forms.Label lab4;
+        private System.Windows.Forms.Label lab5;
+        private System.Windows.Forms.Label lab6;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
